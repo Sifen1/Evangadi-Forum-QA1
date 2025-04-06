5a9266476cdbc377e6760e1690c1074bc47d5729
sifen <sifaman4@gmail.com>
2025-04-06 14:38:24 +0300

    axios

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "https://evangadi-forum-qa1-7.onrender.com/api",
+  baseURL: "https://evangadi-forum-qa1-8.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
